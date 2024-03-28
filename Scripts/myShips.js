ad40b7ffd0844de14c09d2cf3bf0896cf3a0d9f2
HubbePlatteau <hubbe.platteau@student.vives.be>
2024-03-28 12:10:56 +0100

    website 0.1

--- Scripts/myShips.js
+++ Scripts/myShips.js
@@ -1,28 +1,22 @@
-const dotenv=require("dotenv");
-const { json } = require("stream/consumers");
-dotenv.config()
+const DataBase = require('../DataBase.json');
 
-const options = {
+
+function getShips() {
+  const options = {
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + process.env.TOKEN
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + DataBase.token,
     },
   };
 
-
-
-
-fetch('https://api.spacetraders.io/v2/my/ships', options)
-  .then(response => response.json())
-  .then(response => {
-    var ships = (response.data);
-    for (let i = 0; i < ships.length; i++) {
+  fetch("https://api.spacetraders.io/v2/my/ships", options)
+    .then((response) => response.json())
+    .then((response) => {
+      var ships = response.data;
+      for (let i = 0; i < ships.length; i++) {
         console.log(ships[i]);
-    }
-  })
-  .catch(err => console.error(err));
-
-
-
-
- 
+        
+      }
+    })
+    .catch((err) => console.error(err));
+}
